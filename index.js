0e4f592cf1ae78f32fcbda334fdd8fdc45aeab6f
NerdBotDiscord <52212597+NerdBotDiscord@users.noreply.github.com>
2020-09-15 17:44:30 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const Twit = require('twit');
+const T = new Twit({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
+// start stream and track tweets
+const stream = T.stream('statuses/filter', {track: '#JavaScript'});
+// event handler
+stream.on('tweet', tweet => {
+// perform some action here
+});
